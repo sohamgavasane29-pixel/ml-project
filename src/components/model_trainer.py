14a00b22b066a3f3ba7e2353414637d6229bd123
sohamgavasane29-pixel <sohamgavasane29@gmail.com>
2026-06-27 03:51:15 +0530

    hyperparameter tunning done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -28,6 +28,7 @@ class ModelTrainer:
 
     def initiate_model_trainer(self,train_array,test_array):
         try:
+           
             logging.info("splitting ,training & test input data")
             x_train,y_train,x_test,y_test=(
                  train_array[:, :-1],
@@ -36,6 +37,7 @@ class ModelTrainer:
                  test_array[:, -1]
             
              )
+           
             models = {
                "Random Forest": RandomForestRegressor(),
                "Decision Tree": DecisionTreeRegressor(),
@@ -46,9 +48,57 @@ class ModelTrainer:
                "CatBoost Regressor": CatBoostRegressor(verbose=False),
                "AdaBoost Regressor": AdaBoostRegressor(),
             }
+            params = {
 
-            model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models)
+                  "Decision Tree": {
+                  "criterion": ["squared_error", "friedman_mse", "absolute_error", "poisson"],
+                   "splitter": ["best", "random"],
+                   "max_features": ["sqrt", "log2"],
+                 },
+
+    "Random Forest": {
+        "criterion": ["squared_error", "friedman_mse", "absolute_error", "poisson"],
+        "max_features": ["sqrt", "log2", None],
+        "n_estimators": [8, 16, 32, 64, 128, 256],
+    },
+
+    "Gradient Boosting": {
+        "loss": ["squared_error", "huber", "absolute_error", "quantile"],
+        "learning_rate": [0.1, 0.01, 0.05, 0.001],
+        "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+        "criterion": ["squared_error", "friedman_mse"],
+        "n_estimators": [8, 16, 32, 64, 128, 256],
+    },
+
+    "Linear Regression": {},
+
+    "K-Neighbor Regressor": {
+        "n_neighbors": [5, 7, 9, 11],
+        "weights": ["uniform", "distance"],
+        "algorithm": ["ball_tree", "kd_tree", "brute"],
+    },
+
+    "XGBRFRegressor": {
+        "learning_rate": [0.1, 0.01, 0.05, 0.001],
+        "n_estimators": [8, 16, 32, 64, 128, 256],
+    },
+
+    "CatBoost Regressor": {
+        "depth": [6, 8, 10],
+        "learning_rate": [0.01, 0.05, 0.1],
+        "iterations": [30, 50, 100],
+    },
+
+    "AdaBoost Regressor": {
+        "learning_rate": [0.1, 0.01, 0.5, 0.001],
+        "loss": ["linear", "square", "exponential"],
+        "n_estimators": [8, 16, 32, 64, 128, 256],
+    },
+}
+
+            model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models,param=params)
            ## to get best model report
+            print("4. Returned from evaluate_models")
             best_model_score=max(sorted(model_report.values()))
             ##to get tge best model name from dict
             best_model_name=list(model_report.keys())[
--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -20,12 +20,27 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         report={}
+        print("Entered evaluate_models")
         for i in range(len(list(models))):
+            print("started")
             model=list(models.values())[i]
-            model.fit(x_train,y_train)
+            para = param[list(models.keys())[i]]
+            print("Creating GridSearchCV")
+
+            gs = GridSearchCV(model, para, cv=3,verbose=3)
+            print("Calling gs.fit()")
+
+            gs.fit(x_train, y_train)
+            print("GridSearch Finished")
+
+            model.set_params(**gs.best_params_)
+            print("Training final model")
+
+            model.fit(x_train, y_train)
+            print("Predicting")
             y_train_pred=model.predict(x_train)
             y_test_pred=model.predict(x_test)
             test_model_score = r2_score(y_test, y_test_pred)    
