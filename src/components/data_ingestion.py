85af9561d694ec3fb0d8086bb6f45a44a4be4d5b
sohamgavasane29-pixel <sohamgavasane29@gmail.com>
2026-06-27 02:34:42 +0530

    model training done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,9 +5,13 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+## later added to test the transformation
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+## later added to test the model
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -52,4 +56,7 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ =data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modelrtainer=ModelTrainer()
+    print(modelrtainer.initiate_model_trainer(train_arr,test_arr))
